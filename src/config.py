dd84d23b0a6afec7831964a7be2787cc18cd1e61
Gaurang Ahuja <gaurang23@gmail.com>
2026-05-11 11:58:41 -0700

    create config file for column selection

---
+++ src/config.py
@@ -0,0 +1,11 @@
+ID_COLS = ["CNT", "CNTSCHID", "CNTSTUID", "OECD", "STRATUM"]
+
+WEIGHT_COLS = ["W_FSTUWT"] + [f"W_FSTURWT{i}" for i in range(1, 81)]
+
+SCORE_COLS = (
+    [f"PV{i}MATH" for i in range(1, 11)] +
+    [f"PV{i}READ" for i in range(1, 11)] +
+    [f"PV{i}SCIE" for i in range(1, 11)]
+)
+
+KEEP_COLS = ID_COLS + WEIGHT_COLS + SCORE_COLS
